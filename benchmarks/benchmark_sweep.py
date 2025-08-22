8780cbd00bc768e344ddb1d42bb82a3c39d94c5d
Emmanuel Butsana <ebutsana@gmail.com>
2025-08-22 11:02:19 -0400

    Fixed deprecation warning in benchmark_sweep

--- benchmarks/benchmark_sweep.py
+++ benchmarks/benchmark_sweep.py
@@ -59,7 +59,7 @@ def get_processor_name():
         all_info = subprocess.check_output(command, shell=True).decode().strip()
         for line in all_info.split("\n"):
             if "model name" in line:
-                return re.sub( ".*model name.*:", "", line,1)
+                return re.sub( ".*model name.*:", "", line, count=1)
     return ""
 
 ##
